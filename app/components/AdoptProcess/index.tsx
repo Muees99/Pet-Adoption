c6939d827e8d6fabfb7e190ca133dfd244e5fc14
Muees99 <132849693+Muees99@users.noreply.github.com>
2024-06-10 23:57:50 +0100

    Adoption Process Section

---
+++ app/components/AdoptProcess/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function AdoptProcess() {
+  return (
+    <div className=" w-full sm:h-screen h-full bg-[#000]">
+      <div className="max-w-[1440px] mx-auto sm:px-[5%] px-[4%]">
+        <div className="  flex w-full h-full items-center">
+          <div className="  text-white flex flex-col md:flex-row justify-center items-center w-full ">
+            <h2 className="text-white text-3xl md:text-5xl sm:text-2xl  leading-[140%] font-semibold">
+              Adoption Process Information
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AdoptProcess;
