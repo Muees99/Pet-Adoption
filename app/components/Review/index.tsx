c4e6eb3251ed2f023cec921532080991f39b936c
Muees99 <132849693+Muees99@users.noreply.github.com>
2024-06-15 22:52:50 +0100

    Review Component

--- app/components/Review/index.tsx
+++ app/components/Review/index.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 function Review() {
   return (
-    <div>Review</div>
-  )
+    <div className="w-full min-h-screen bg-[#000]">
+      {/* Review */}
+      <div className="max-w-[1440px] mx-auto sm:px-[5%] px-[4%]">
+        <div className="  flex w-full h-full items-center  justify-center">
+          <h2 className=" text-white text-2xl md:text-4xl sm:text-2xl leading-[140%] font-semibold mt-4 ">
+            Happy Clients Review
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Review
